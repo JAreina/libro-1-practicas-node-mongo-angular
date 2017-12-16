053b3032d47c53981ebd3c889924126143c270be
JAreina <jareinafdez@gmail.com>
2017-12-16 09:58:40 +0100

    procees modulo

---
+++ 12-process-modulo/child-process-exec-file.js
@@ -0,0 +1,21 @@
+ 
+ //child_process.execFile(file, args, options, callback)
+ // no va en linux--  'ping.exe', ['-n', '1', 'google.com']
+ 
+ var childProcess = require('child_process');
+ var options = {maxBuffer:100*1024, encoding:'utf8', timeout:5000};
+ var child = childProcess.execFile('ping.exe', ['-n', '1', 'google.com'],
+                 options, function (error, stdout, stderr) {
+                            if (error) {
+                            console.log(error.stack);
+                            console.log('Error Code: '+error.code);
+                            console.log('Error Signal: '+error.signal);
+                            }
+                            console.log('Results: \n' + stdout);
+                            if (stderr.length){
+                            console.log('Errors: ' + stderr);
+                            }
+                    });
+ child.on('exit', function (code) {
+            console.log('Child completed with code: '+code);
+ });
